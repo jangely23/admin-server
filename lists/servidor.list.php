e4971ba592768b2de2bd370a2ed866967fe7446f
jessica leonel <jangely23@hotmail.com>
2021-09-15 18:20:22 -0500

    se completa el archivo DTO y se inicia el DAO de la entidad servidor

--- lists/servidor.list.php
+++ lists/servidor.list.php
@@ -1,7 +1,7 @@
 <?php 
-require "./config/conexion.php";
-require "./class/conexion.class.php";
-require "./class/servidorDTO.class.php";
-require "./class/servidorDAO.class.php";
+require "../config/conexion.php";
+require "../class/conexion.class.php";
+require "../class/servidorDTO.class.php";
+require "../class/servidorDAO.class.php";
 
 ?>
