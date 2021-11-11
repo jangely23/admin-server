0b7cedbf41a4c4f26481fb136d303b3dbe70aeb2
jangely23 <jangely23@hotmail.com>
2021-11-11 18:05:15 -0500

    clase cliente producto pago

--- class/cliente_producto_pagoDAO.class.php
+++ class/cliente_producto_pagoDAO.class.php
@@ -9,6 +9,22 @@ class cliente_producto_cobroDAO extends conexion{
     function __construct($conexion) {
         parent::__construct($conexion);
     }
+
+    function getById(int $id_cliente_producto_pago){
+            $query = sprintf("SELECT * FROM cliente_producto_pago WHERE id_cliente_producto_pago = '%d'",$id_cliente_producto_pago);
+            $result = $this->getConexion()->query($query);
+
+            if($result){
+                if($result->num_rows != 0){
+                    return $result->fetch_object();
+                }else{
+                    return 0;
+                }
+            }else{
+                throw new Exception("Error al intentar getById() en cliente_producto_pagoDAO");
+            }
+    }
+    
 }
 
 ?>
